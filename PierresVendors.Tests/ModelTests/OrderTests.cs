7c7e7f53a5d5c007140852864a0f54ee6312225d
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-14 10:05:23 -0700

    prepares test and model files for first test

---
+++ PierresVendors.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,9 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresVendors.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PierresVendors.Tests
+{
+  
+}
---
+++ PierresVendors/Models/Vendor.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace PierresVendors.Models
+{
+  public class Vendor
+  {
+    
+  }
+}
