d53ff79d35f5a2b051a550b658d44c9f426ad95d
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-14 11:44:24 -0700

    passes second test, passes green phase, returns delivery date

--- PierresVendors/Models/Order.cs
+++ PierresVendors/Models/Order.cs
@@ -12,12 +12,12 @@ namespace PierresVendors.Models
     public string DeliveryDate { get; set; }
     private static List<Order> _orders = new List<Order> { };
 
-    public Order(int ordernumber, int quantity, string item, string deliveryDate)
+    public Order(int orderNumber, int quantity, string item, string deliveryDate)
     {
-      OrderNumber = 9;
-      Quantity = 9;
-      Item = "croissandwiches";
-      DeliveryDate = "daily";
+      OrderNumber = orderNumber;
+      Quantity = quantity;
+      Item = item;
+      DeliveryDate = deliveryDate;
     }
   }
 }
