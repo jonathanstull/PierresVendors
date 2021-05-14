81100c6b6a2102ce04d57c26076cca796d5d6c1d
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-14 11:37:13 -0700

    adds and passes first test, object typeof

--- PierresVendors.Tests/ModelTests/OrderTests.cs
+++ PierresVendors.Tests/ModelTests/OrderTests.cs
@@ -5,5 +5,20 @@ using System.Collections.Generic;
 
 namespace PierresVendors.Tests
 {
-  
+  [TestClass]
+  public class OrderTests
+  {
+
+    [TestMethod]
+    public void FirstOrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      // Arrange
+      Order newOrder = new Order(01, 300, "old fashioneds", "5.14.21");
+
+      // Assert
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+    
+  }
 }
--- PierresVendors/Models/Order.cs
+++ PierresVendors/Models/Order.cs
@@ -5,6 +5,19 @@ namespace PierresVendors.Models
 {
   public class Order
   {
-    
+
+    public int OrderNumber { get; set; }
+    public int Quantity { get; set; }
+    public string Item { get; set; }
+    public string DeliveryDate { get; set; }
+    private static List<Order> _orders = new List<Order> { };
+
+    public Order(int OrderNumber, int Quantity, string Item, string DeliveryDate)
+    {
+      OrderNumber = 9;
+      Quantity = 9;
+      Item = "croissandwiches";
+      DeliveryDate = "daily";
+    }
   }
 }
